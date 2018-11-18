e24a89c0af31ee52d1a55cb46555326468154a10
ROIMER PERAZA <rperaza@isarq.com>
2018-11-17 22:14:27 -0400

    changes

--- src/app/explorer/components/all-transactions/all-transactions.component.ts
+++ src/app/explorer/components/all-transactions/all-transactions.component.ts
@@ -36,44 +36,6 @@ export class AllTransactionsComponent implements OnInit, AfterViewInit {
   elements = [];
   headElements = ['Block Height', 'Timestamp', 'Harvester/Forger', 'Difficulty', 'Fees', 'Export CSV'];
 
-
-  options = {
-    fieldSeparator: ',',
-    quoteStrings: '`',
-    decimalseparator: '.',
-    showLabels: true,
-    headers: [],
-    showTitle: true,
-    title: 'asfasf',
-    useBom: false,
-    removeNewLines: true,
-    keys: ['approved', 'age', 'name']
-  };
-  data = [
-    {
-      name: `Test, 1`,
-      age: 13,
-      average: 8.2,
-      approved: true,
-      description: `using 'Content here, content here' `
-    },
-    {
-      name: 'Test 2',
-      age: 11,
-      average: 8.2,
-      approved: true,
-      description: `using 'Content here, content here' `
-    },
-    {
-      name: 'Test 3',
-      age: 10,
-      average: 8.2,
-      approved: true,
-      description: `using 'Content here, content here' `
-    }
-  ];
-
-
   constructor(
     private tableService: MdbTableService,
     private cdRef: ChangeDetectorRef
